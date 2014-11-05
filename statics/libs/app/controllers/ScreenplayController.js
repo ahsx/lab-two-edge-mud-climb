95578edfd5a40c382847021397dbe3c603045dd4
ahsx <ahsx@ahsx.be>
2014-11-05 14:02:40 +0100

    Improve the save to bitmap

--- statics/libs/app/controllers/ScreenplayController.js
+++ statics/libs/app/controllers/ScreenplayController.js
@@ -3,6 +3,8 @@ angular
 	.controller('ScreenplayController', ['$scope', '$rootScope', '$element',  '$window', 'CanvasService', function($scope, $rootScope, $element, $window, CanvasService)
 	{
 		CanvasService.init( $element.find('canvas'), jQuery($element).find('.container'), $window );
+		CanvasService.setDescription( $scope.title, $scope.subtitle ); 
+		
 		$rootScope.saveCanvas = function()
 		{
 			$rootScope.saveURL = CanvasService.getURL();
